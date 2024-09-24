7f43548a238411aa3dd74fc11165b5df18a73643
Paolodicaprio <dagbapaolo@gmail.com>
2024-09-24 12:21:18 +0100

    Add bouton to article page

--- pages/[...slug].tsx
+++ pages/[...slug].tsx
@@ -1,11 +1,11 @@
 import { GetStaticPathsResult, GetStaticPropsResult } from "next"
 import Head from "next/head"
 import { DrupalNode } from "next-drupal"
-
 import { drupal } from "lib/drupal"
 import { NodeArticle } from "components/node--article"
 import { NodeBasicPage } from "components/node--basic-page"
 import { Layout } from "components/layout"
+import { useRouter } from "next/router"
 
 const RESOURCE_TYPES = ["node--page", "node--article"]
 
@@ -14,6 +14,8 @@ interface NodePageProps {
 }
 
 export default function NodePage({ resource }: NodePageProps) {
+  const router = useRouter()
+
   if (!resource) return null
 
   return (
@@ -22,8 +24,26 @@ export default function NodePage({ resource }: NodePageProps) {
         <title>{resource.title}</title>
         <meta name="description" content="A Next.js site powered by Drupal." />
       </Head>
+
       {resource.type === "node--page" && <NodeBasicPage node={resource} />}
       {resource.type === "node--article" && <NodeArticle node={resource} />}
+
+      {/* Ajouter le bouton Retour ici */}
+      <button
+        onClick={() => router.back()}
+        style={{
+          position: 'fixed',
+          bottom: '20px',
+          right: '20px',
+          padding: '10px 20px',
+          backgroundColor: 'gray',
+          color: 'white',
+          borderRadius: '5px',
+          boxShadow: '0 2px 5px rgba(0, 0, 0, 0.3)',
+        }}
+      >
+        Retour
+      </button>
     </Layout>
   )
 }
@@ -63,16 +83,10 @@ export async function getStaticProps(
     }
   )
 
-  // At this point, we know the path exists and it points to a resource.
-  // If we receive an error, it means something went wrong on Drupal.
-  // We throw an error to tell revalidation to skip this for now.
-  // Revalidation can try again on next request.
   if (!resource) {
     throw new Error(`Failed to fetch resource: ${path.jsonapi.individual}`)
   }
 
-  // If we're not in preview mode and the resource is not published,
-  // Return page not found.
   if (!context.preview && resource?.status === false) {
     return {
       notFound: true,
